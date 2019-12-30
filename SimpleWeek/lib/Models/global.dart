ff0bea3e9bfadc7d9416a3419ab767670f619eaa
Martin Nygaard <marandny@gmail.com>
2019-12-30 19:39:56 +0100

    Created bottom selection navbar

---
+++ SimpleWeek/lib/Models/global.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+Color greenNormal = new Color(0xFF1AB151);
+Color greenDark = new Color(0xFF118F3F);
+Color darkGreyBackground = new Color (0xFF282A39);
+Color greyNormal = new Color (0xFF333541);
+Color greyHighlight = new Color (0xFF61636B);
+
+TextStyle headerStyle = new TextStyle(
+  fontFamily: 'Yu Gothic UI',
+  color: Colors.white,
+  fontSize: 54.0,
+);
+TextStyle navbarStyle = new TextStyle(
+  fontFamily: 'Yu Gothic UI',
+  color: Colors.white,
+  fontSize: 14.0
+);
+  
